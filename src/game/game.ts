f4e792e555392936f58f06148804aa1778110e4c
troches <trystan.rcs4@gmail.com>
2023-12-07 23:43:35 +0100

    feat: init board

---
+++ src/game/game.ts
@@ -0,0 +1,19 @@
+import { Coordinates } from "./coordinates";
+import { Grid } from "./grid";
+
+export class Game {
+    players: Array<string>;
+    grids: Array<Grid>;
+    currentPlayer: number;
+    readonly BOAT_SIZES = [5, 4, 3, 3, 2];
+
+    constructor(players: Array<string>) {
+        this.players = players;
+        this.grids = players.map(() => new Grid());
+        this.currentPlayer = Math.floor(Math.random() * players.length);
+    }
+
+    initBoat(currentPlayer: number, currentBoat: number, coordinates: Array<Coordinates>): boolean {
+        return coordinates.length === this.BOAT_SIZES[currentBoat] && this.grids[currentPlayer].addBoat(coordinates);
+    }
+}
--- src/game/grid.ts
+++ src/game/grid.ts
@@ -1,43 +1,63 @@
-import { GridState } from './GridState';
-import { Coordinates } from './coordinates';
-import { HitState } from './hit-state';
+import { GridState } from "./grid-state";
+import { Coordinates } from "./coordinates";
+import { HitState } from "./hit-state";
 
-const GRID_SIZE = 10; 
+const GRID_SIZE = 10;
 
 export class Grid {
-    matrix!: Array<Array<GridState>>
-    hitMap!: Map<Coordinates, number>
-    boatSizes!: Array<number>
-    hitCells!: Array<Coordinates>
+  matrix!: Array<Array<GridState>>;
+  hitMap!: Map<Coordinates, number>;
+  boatSizes!: Array<number>;
+  hitCells!: Array<Coordinates>;
 
-    constructor() {
-        this.matrix = new Array(GRID_SIZE)
-            .fill(new Array(GRID_SIZE).fill(GridState.Water));
-        this.hitMap = new Map();
-        this.boatSizes = new Array();
-        this.hitCells = new Array();
+  constructor() {
+    this.matrix = new Array(GRID_SIZE).fill(
+      new Array(GRID_SIZE).fill(GridState.Water)
+    );
+    this.hitMap = new Map();
+    this.boatSizes = new Array();
+    this.hitCells = new Array();
+  }
+
+  addBoat(coordinates: Array<Coordinates>): boolean {
+    const result = coordinates.every((coord) => {
+      const { row, col } = coord;
+      return (
+        row >= 0 &&
+        row < GRID_SIZE &&
+        col >= 0 &&
+        col < GRID_SIZE &&
+        this.matrix[row][col] === GridState.Water
+      );
+    });
+
+    if (result) {
+      coordinates.forEach((coord) => {
+        this.matrix[coord.row][coord.col] = GridState.Boat;
+        this.hitMap.set(coord, coordinates.length);
+      });
+      this.boatSizes.push(coordinates.length);
     }
 
-    addBoat(coordinates: Array<Coordinates>) {
-        coordinates.forEach(coord => {
-            this.matrix[coord.row][coord.col] = GridState.Boat;
-            this.hitMap.set(coord, coordinates.length);
-        });
-        this.boatSizes.push(coordinates.length);
+    return result;
+  }
+
+  hitCell(coordinates: Coordinates): HitState {
+    if (this.hitMap.has(coordinates)) {
+      return HitState.Invalid;
     }
 
-    hitCell(coordinates: Coordinates): HitState {
-        if (this.hitMap.has(coordinates)) {
-            return HitState.Invalid;
-        }
+    this.hitCells.push(coordinates);
+    const boatId = this.hitMap.get(coordinates);
 
-        this.hitCells.push(coordinates);
-        const boatId = this.hitMap.get(coordinates);
-
-        if (boatId === undefined) {
-            return HitState.Miss;
-        }
-
-        return --this.boatSizes[boatId] === 0 ? HitState.Sunk : HitState.Hit;
+    if (boatId === undefined) {
+      return HitState.Miss;
     }
+
+    return --this.boatSizes[boatId] === 0
+      ? this.boatSizes.every((size) => size === 0)
+        ? HitState.Win
+        : HitState.Sunk
+      : HitState.Hit;
+  }
 }
