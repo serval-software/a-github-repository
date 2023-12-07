085bdbea9fc44beeaafaa54166c1f951f39f2bda
troches <trystan.rcs4@gmail.com>
2023-12-08 00:15:04 +0100

    feat: init cell display

--- src/components/cell.tsx
+++ src/components/cell.tsx
@@ -1,5 +1,20 @@
-import { Grid } from "@mui/material"
+import React from "react";
+import { Grid, Typography } from "@mui/material";
 
-export const Cell = () => {
-    return (<Grid container></Grid>)
+export const Cell = ({cellType, boatColor}) => {
+
+    const displayCell = () => {
+        // TODO 000 : if cellType is Boat, add color to style
+        return (<Typography> {cellType} </Typography>);
+    }
+
+    return (
+    <Grid container>
+        <Grid item xs={12}>
+            {
+                displayCell()
+            }
+        </Grid>
+    </Grid>
+    )
 }
